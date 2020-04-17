3cba9090dabdf7c231e991c544043e920f82d004
ethan <e@coip.me>
2020-04-17 13:02:33 -0500

    bfs

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+var (
+	s_ack = "👍"
+	c_ack = []byte(s_ack + "\n")
+
+	port string
+)
+
+func main() {
+
+	if port = os.Getenv("port"); len(port) == 0 {
+		port = ":8080"
+	}
+
+	print("binding to port: [" + port + "] ..." + "\n")
+
+	print(http.ListenAndServe(port,
+		(http.HandlerFunc)(func(w http.ResponseWriter, r *http.Request) {
+			print(s_ack)
+			w.Header().Add("Content-Type", "application/javascript; charset=utf-8")
+			w.Write(c_ack)
+		}),
+	))
+
+}
